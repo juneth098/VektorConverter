ae999fb34afce9b80cf8ce0066f95d21864efeb5
Juneth Moreno <juneth098@gmail.com>
2025-12-09 16:41:10 +0800

    in J750, need smace per bit in vector line.

--- scripts/metadata.py
+++ scripts/metadata.py
@@ -3,7 +3,14 @@ author = "Juneth Viktor Ellon Moreno"
 #script_ver = "0.1"
 #initial beta release of VektorConverter
 #convert Design Vectors to ATE-ready pattern files
-script_ver = "1.0"
+#script_ver = "1.0"
 #Official Release for Production use
 #no known-issues
 #documentation is ready
+script_ver = "1.1"
+#Official Release for Production use
+# Major Issue: Error in Pattern Compiler; Cannot generate pat.gz file
+# Fix: Setup- CMF or Vector pins must match the current Pin Map sheet in the IG-XL Workbook
+#      vec2ate.py - must contain at least 1 space between bits
+#      ate2vec.py - updated to detect the spaces between vectors
+# Verification: Done checking vec/cmf/.atp/pat. Tried the atp with PatternCompiler, no more issues
